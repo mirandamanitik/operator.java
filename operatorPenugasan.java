db87206fee6827d01c654c032288db6266a70bb2
mirandamanitik <manitikmiranda@gmail.com>
2025-01-21 00:30:26 +0800

    Program Sederhana Operator tugas 2

---
+++ operatorPenugasan.java
@@ -0,0 +1,18 @@
+public class operatorPenugasan { public static void main(String[] args) {
+    int a = 5;
+    int b = 2;
+    int c = 0;
+    c = a + b;
+    System.out.println("c = a + b = " + c);
+    c = a;
+    System.out.println(" c = a = " + c);
+    c += a;
+    System.out.println(" c += a = " + c);
+    c -= a;
+    System.out.println("c -= a=" + c);
+    c *= a;
+    System.out.println("c *= a =" + c);
+    
+}
+    
+}
